fa2d421292c6b4dc2aa06e9d8ef88b9587b500af
Khondakar Afridi <afridi.khondakar@gmail.com>
2024-12-10 10:12:41 +0600

    Sales report controller initialized

--- lib/service/dependency_injection_service.dart
+++ lib/service/dependency_injection_service.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:pdf_printer/controllers/order_list_controller.dart';
 import 'package:pdf_printer/controllers/product_list_controller.dart';
+import 'package:pdf_printer/controllers/sales_report_controller.dart';
 import 'package:pdf_printer/service/network/network-service-c.dart';
 
 class DependencyInjection {
@@ -8,5 +9,6 @@ class DependencyInjection {
     Get.put<NetworkService>(NetworkService(), permanent: true);
     Get.put<OrderListController>(OrderListController(), permanent: true);
     Get.put<ProductListController>(ProductListController(), permanent: true);
+    Get.put<SalesReportController>(SalesReportController(), permanent: true);
   }
 }
