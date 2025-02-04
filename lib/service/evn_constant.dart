0b6c247c137ef91274b718768ca49f8cfe01ae46
Khondakar Afridi <afridi.khondakar@gmail.com>
2025-02-04 10:02:03 +0600

    Enabled evn consts

--- lib/service/evn_constant.dart
+++ lib/service/evn_constant.dart
@@ -1,29 +1,28 @@
-// class EvnConstant {
-//   static const String baseUrl = String.fromEnvironment(
-//     'BASE_URL',
-//     defaultValue: '',
-//   );
-
-//   static const String consumerKey = String.fromEnvironment(
-//     'CONSUMER_KEY',
-//     defaultValue: '',
-//   );
-
-//   static const String consumerSecret = String.fromEnvironment(
-//     'CONSUMER_SECRET',
-//     defaultValue: '',
-//   );
-
-//   static const String version = String.fromEnvironment(
-//     'VERSION',
-//     defaultValue: '',
-//   );
-
-// }
-
 class EvnConstant {
-  static const String baseUrl = 'https://currytown.ca';
-  static const String consumerKey = 'ck_0d928d6a68dfdcb7e6d0268457eecfc9cce04582';
-  static const String consumerSecret = 'cs_9a9d668c7b322f8c1910cc4f66d8820f5e31c720';
-  static const String version = 'wc/v3';
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: '',
+  );
+
+  static const String consumerKey = String.fromEnvironment(
+    'CONSUMER_KEY',
+    defaultValue: '',
+  );
+
+  static const String consumerSecret = String.fromEnvironment(
+    'CONSUMER_SECRET',
+    defaultValue: '',
+  );
+
+  static const String version = String.fromEnvironment(
+    'VERSION',
+    defaultValue: '',
+  );
 }
+
+// class EvnConstant {
+//   static const String baseUrl = 'https://currytown.ca';
+//   static const String consumerKey = 'ck_0d928d6a68dfdcb7e6d0268457eecfc9cce04582';
+//   static const String consumerSecret = 'cs_9a9d668c7b322f8c1910cc4f66d8820f5e31c720';
+//   static const String version = 'wc/v3';
+// }
